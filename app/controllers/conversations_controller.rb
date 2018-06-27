b174dab189e5f1c28d6430ac099baa4b7b6b877f
Takanori Sugita <hitsujiniku.sugita@gmail.com>
2018-06-27 22:27:26 +0900

    表示するconversationを1つに
    TODO: conversationsっていう名前が残ってしまっているので要修正

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,5 @@
 class ConversationsController < ApplicationController
   def create
-    # query paramsを?user_id=curernt_user.idにして会話状態をget
     @conversation = Conversation.get(current_user.id, params[:user_id])
 
     add_to_conversations unless conversated?
@@ -10,21 +9,10 @@ class ConversationsController < ApplicationController
     end
   end
 
-  def close
-    @conversation = Conversation.find(params[:id])
-
-    session[:conversations].delete(@conversation.id)
-
-    respond_to do |format|
-      format.js
-    end
-  end
-
   private
 
-  # アクティブなconversation_idを格納
   def add_to_conversations
-    session[:conversations] ||= []
+    session[:conversations] = []
     session[:conversations] << @conversation.id
   end
 
