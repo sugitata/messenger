82567fddeaf139447eab97e347ffe9e3ca9860d0
Takanori Sugita <hitsujiniku.sugita@gmail.com>
2018-06-28 19:07:06 +0900

    conversaitonのセッション管理をやめた

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,21 +2,8 @@ class ConversationsController < ApplicationController
   def create
     @conversation = Conversation.get(current_user.id, params[:user_id])
 
-    add_to_conversation unless conversated?
-
     respond_to do |format|
       format.js
     end
-  end
-
-  private
-
-  def add_to_conversation
-    session[:conversation] = []
-    session[:conversation] << @conversation.id
-  end
-
-  def conversated?
-    session[:conversation].include?(@conversation.id)
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    session[:conversation] = []
-
     @users = User.all.where.not(id: current_user)
   end
 end
